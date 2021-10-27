c1986190f4fd2806949b4d2e8ae30fddc899e0aa
geraldlee2001 <geraldlee168168@gmail.com>
2021-10-27 21:47:28 +0800

    refactor: refactor condition statement

--- src/2FA.ts
+++ src/2FA.ts
@@ -75,14 +75,13 @@ export default class twoFA {
 
     // Make sure the code matches the optLength
     // Add zeros to the beginning if int parsing ommited any zeros
-    if (token.length !== otpLength){
-          for (let i = token.length; i < otpLength; i += 1) {
-            token = `0${token}`;
-        }
-
-        return token;
-        }
-        return token;
+    if (token.length < otpLength){
+      for (let i = token.length; i < otpLength; i += 1) {
+        token = `0${token}`;
+      }
+      return token;
+    }
+      return token;
   }
 
   /**
